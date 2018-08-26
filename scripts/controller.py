7d86f034b484c773f2c4e5e005a2f74864d66ed0
RJ722 <rj722@protonmail.com>
2018-08-26 17:28:35 +0000

    Complete Assignment 2

---
+++ scripts/controller.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import LaserScan
+
+
+def _callback(msg):
+    rospy.loginfo(min(msg.ranges))
+
+
+def listen():
+    # Gather config
+    name = rospy.get_param('name')
+    queue_size = rospy.get_param('queue_size')
+
+    rospy.init_node(name, anonymous=True)
+    rospy.Subscriber('/scan', LaserScan, _callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listen()
